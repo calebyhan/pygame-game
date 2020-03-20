6179fb300653c81b6c82c6167c64a2973512b1e1
Shadowfax1162 <calebhantech@gmail.com>
2020-03-20 11:01:57 -0400

    Made rect move

--- main.py
+++ main.py
@@ -9,13 +9,31 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
-
-pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((225, 225, 225))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -1)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 1)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+
+pygame.init()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
 
 running = True
 
@@ -27,17 +45,16 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    pressed_keys = pygame.key.get_pressed()
+
+    player.update(pressed_keys)
+
+    screen.fill((0, 0, 0))
 
     surf = pygame.Surface((50, 50))
 
     surf.fill((0, 0, 0))
-    rect = surf.get_rect()
 
-    surf_center = (
-        (SCREEN_WIDTH-surf.get_width()) / 2,
-        (SCREEN_HEIGHT - surf.get_height()) / 2
-    )
+    screen.blit(player.surf, player.rect)
 
-    screen.blit(surf, surf_center)
     pygame.display.flip()
