8efe09910a52dd44690e1248aac5ee8f58bb3046
Caleb Han <59373603+calebyhan@users.noreply.github.com>
2022-07-17 19:06:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import pygame
+'''
+Spaceship pygame
+Author: Caleb Han
+'''
 
+# imports
+import pygame
 import random
 
+# get keystrokoes
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -11,16 +17,20 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+
+# set up screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+# set up player class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((225, 225, 225))
         self.rect = self.surf.get_rect()
-
+    
+    # move the player
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -1)
@@ -40,6 +50,7 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
 
+# set up enemy class
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
@@ -53,29 +64,37 @@ class Enemy(pygame.sprite.Sprite):
         )
         self.speed = random.randint(5, 20)
 
+    # move enemy
     def update(self):
         self.rect.move_ip(-self.speed, 0)
         if self.rect.right < 0:
             self.kill()
 
+# run game
 pygame.init()
 
+# set up a clock
 clock = pygame.time.Clock()
 
+# set up the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# add an enemy
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 250)
 
+# add player
 player = Player()
 
+# set up variables
 enemies = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
+# get main running loop
 running = True
-
 while running:
+    # quit game or add enemy events
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -88,24 +107,26 @@ while running:
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
 
+    # get pressed keys and move
     pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
-
+    
+    # move enemies
     enemies.update()
-
+    
+    # screen manipulation
     screen.fill((0, 0, 0))
-
     surf = pygame.Surface((50, 50))
-
     surf.fill((0, 0, 0))
 
+    # sprite manipulation
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
 
+    # detect collision
     if pygame.sprite.collideandy(player, enemies):
         player.kill()
         running = False
 
     pygame.display.flip()
-
     clock.tick(1)
